7f4fda36c619198d8d23eac0d35f9c5c679f629b
Abel T <abelt.codes@gmail.com>
2022-01-24 09:01:01 -0700

    calculator

---
+++ rb101/lesson-2/calculator.rb
@@ -0,0 +1,6 @@
+#Build a command line calculator program that does the following:
+
+#asks for two numbers
+#asks for the type of operation to perform: add, subtract, multiply or divide
+#displays the result
+#Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output. Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
