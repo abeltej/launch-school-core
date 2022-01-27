cc1f415f0029671e152ac5ca492c03d8f375d940
Abel T <abelt.codes@gmail.com>
2022-01-27 14:48:52 -0700

    psuedo code cont

--- rb101/lesson-2/psuedocode.rb
+++ rb101/lesson-2/psuedocode.rb
@@ -101,7 +101,7 @@ everyOther([1,4,7,2,5]) # => [1,7,5]
 
 # a method that determines the index of the 3rd occurrence of a given character in a string. For instance, if the given character is 'x' and the string is 'axbxcdxex', the method should return 6 (the index of the 3rd 'x'). If the given character does not occur at least 3 times, return nil.
 
-# a method that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should becoome the elements at the odd indexes. For instance:
+# a method that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes. For instance:
 merge([1, 2, 3], [4, 5, 6]) # => [1, 4, 2, 5, 3, 6]
 
 # You may assume that both array arguments have the same number of elements.
