7e851763e187b1b6578a7e0793d41c5038bf8c91
Abel T <abelt.codes@gmail.com>
2022-02-01 14:34:45 -0700

    refactor calc case operator

--- rb101/lesson-2/refactor-calc.rb
+++ rb101/lesson-2/refactor-calc.rb
@@ -5,24 +5,25 @@ def prompt(message)
   
 end
 
-Kernel.puts("Welcome to Calculator!")
-Kernel.puts ("Whats the first number")
+prompt("Welcome to Calculator!")
+prompt("Whats the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("Whats the second number")
+prompt("Whats the second number?")
+
 number2 = Kernel.gets().chomp()
+prompt("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
 
-Kernel.puts("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
 operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == "2"
-  result = number1.to_i() - number2.to_i()
-elsif operator == "3"
-  result = number1.to_i() * number2.to_i()
-else operator == "4"
-  result = number1.to_f() / number2.to_f()
+result = case operator
+             when '1' 
+              number1.to_i() + number2.to_i()
+            when '2' 
+              number1.to_i() - number2.to_i()
+            when '3' 
+              number1.to_i() * number2.to_i()
+            when '4' 
+              number1.to_f() / number2.to_f()
 end
 
-Kernel.puts("The results is #{result}")
+prompt("The results is #{result}")
