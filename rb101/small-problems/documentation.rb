3da53c0e0459085b769c3ef71c24f4e5aec8c48d
Abel T <abelt.codes@gmail.com>
2022-02-01 15:47:23 -0700

    small problems docs

--- rb101/small-problems/documentation.rb
+++ rb101/small-problems/documentation.rb
@@ -1,6 +1,8 @@
 # Locate the ruby documentation for methods File::path and File#path. How are they different?
 
-# File::path
+# Both methods can be found on the documentation page for the File class in the Core API section.
+
+# File::path is a class method
 
 
-# File#path
+# File#path is an instance method
