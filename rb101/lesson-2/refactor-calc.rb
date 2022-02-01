a52a1de9445e6c6c3c311c8192b1724d70dfcf20
Abel T <abelt.codes@gmail.com>
2022-02-01 12:52:27 -0700

    refactor calc

---
+++ rb101/lesson-2/refactor-calc.rb
@@ -0,0 +1,23 @@
+# Walk-through: Refactoring Calculator
+
+Kernel.puts("Welcome to Calculator!")
+Kernel.puts ("Whats the first number")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("Whats the second number")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == "2"
+  result = number1.to_i() - number2.to_i()
+elsif operator == "3"
+  result = number1.to_i() * number2.to_i()
+else operator == "4"
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The results is #{result}")
