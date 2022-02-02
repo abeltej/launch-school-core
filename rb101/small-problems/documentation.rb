16cf8c29835da8ed31d3d7bac3feae78e4a3dfce
Abel T <abelt.codes@gmail.com>
2022-02-02 13:42:03 -0700

    small problems docs #4

--- rb101/small-problems/documentation.rb
+++ rb101/small-problems/documentation.rb
@@ -55,3 +55,10 @@ end
 my_method(4, 5, 6)
 
 # Use the ruby documentation to determine what this code will print.
+
+
+# Mandatory Blocks
+
+# The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. Assume you have the following code: 
+a = [1, 4, 8, 11, 15, 19]
+# How would you search this Array to find the first element whose value exceeds 8?
