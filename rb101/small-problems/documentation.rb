279f8ad120bb55b02029c6de7ecc52875efa8ca8
Abel T <abelt.codes@gmail.com>
2022-02-03 13:42:13 -0700

    small problems docs #6 cont

--- rb101/small-problems/documentation.rb
+++ rb101/small-problems/documentation.rb
@@ -131,3 +131,11 @@ fetch(index) { |index| block } → obj
 # Math
 
 # Integer and Float inherit from Numeric. Numeric is basically the superclass of all numeric types, while Math is a module that supplies a lot of number crunching functionality. Choosing the right documentation to look at is difficult, so, for now, be aware that you may have to look in all these locations.
+
+# As it happens, you can find #step described in the Numeric class documentation. #step has 4 calling signatures, but we're interested in the first listed. In this version, #step takes two keyword arguments (also called named arguments), :by and :to (keyword argument names are symbols). This shows up in our method signature:
+
+step(by: step, to: limit) { |i| block } → self
+
+# The argument named :by is a "step" value, while the argument named :to is a limit. The text below the signature shows that the "step" value indicates how much to increment the index by with each iteration, while "limit" is the largest value; if the index exceeds this value, the loop exits.
+
+# We can also see that #step takes a loop in this first form, and that it will execute the block for each value in the iteration.
