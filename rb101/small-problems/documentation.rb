6712bf28b86ed175e3443ba2ffc65718c8272ae4
Abel T <abelt.codes@gmail.com>
2022-02-03 14:57:29 -0700

    small problems docs #6 clean

--- rb101/small-problems/documentation.rb
+++ rb101/small-problems/documentation.rb
@@ -139,3 +139,5 @@ step(by: step, to: limit) { |i| block } → self
 # The argument named :by is a "step" value, while the argument named :to is a limit. The text below the signature shows that the "step" value indicates how much to increment the index by with each iteration, while "limit" is the largest value; if the index exceeds this value, the loop exits.
 
 # We can also see that #step takes a loop in this first form, and that it will execute the block for each value in the iteration.
+
+# Given all this information, we can see that the code will print 5 on its first iteration, 8 on the second iteration, and then quit because 11 > 10
