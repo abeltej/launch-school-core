8ce1aa5f30c77de767dfa49732e55aa60fd11687
Abel T <abelt.codes@gmail.com>
2022-02-16 13:32:11 -0700

    Refactor calcu

--- rb101/lesson-2/refactor-calc.rb
+++ rb101/lesson-2/refactor-calc.rb
@@ -6,23 +6,25 @@ def prompt(message)
 end
 
 prompt("Welcome to Calculator!")
+
+
 prompt("Whats the first number?")
 number1 = Kernel.gets().chomp()
 
 prompt("Whats the second number?")
-
 number2 = Kernel.gets().chomp()
-prompt("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
 
+prompt("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
 operator = Kernel.gets().chomp()
+
 result = case operator
              when '1' 
               number1.to_i() + number2.to_i()
-            when '2' 
+             when '2' 
               number1.to_i() - number2.to_i()
-            when '3' 
+             when '3' 
               number1.to_i() * number2.to_i()
-            when '4' 
+             when '4' 
               number1.to_f() / number2.to_f()
 end
 
