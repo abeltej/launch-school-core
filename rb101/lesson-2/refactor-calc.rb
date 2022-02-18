d2dadade45a1c8acef56502dfeba82f7e6cce453
Abel T <abelt.codes@gmail.com>
2022-02-17 18:07:28 -0700

    ref calc

--- rb101/lesson-2/refactor-calc.rb
+++ rb101/lesson-2/refactor-calc.rb
@@ -4,15 +4,40 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    "Multiplying"
+  when '4'
+    'Dividing'
+  end
+end 
 def valid_number?(num)
   num.to_i() != 0
 end
 
-prompt("Welcome to Calculator!")
+prompt("Welcome to Calculator! Enter your name:")
+
+name = ''
+loop do 
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("make sure to yse a valid name.")
+  else
+    break
+  end
+end
+
+prompt("hi #{name}")
 
 loop do # main loop
 
-  number1 = ""
+  number1 = ''
   loop do 
     prompt("Whats the first number?")
     number1 = Kernel.gets().chomp()
@@ -24,7 +49,7 @@ loop do # main loop
     end
   end
 
-  number = ''
+  number2 = ''
   loop do
     prompt("Whats the second number?")
     number2 = Kernel.gets().chomp()
@@ -36,10 +61,30 @@ loop do # main loop
     end
   end
 
-  prompt("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
-  operator = Kernel.gets().chomp()
+prompt("What operation would you like to perform? 1. add 2. subtract 3. multiple 4. divide")
+operator_prompt = <<-MSG 
+  what operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply 
+  4) divide 
+  MSG
 
-    result = case operator
+prompt(operator_prompt)
+operator = ''
+
+loop do 
+  operator = Kernel.gets().chomp()
+  
+  if %w( 1 2 3 4).include?(operator)
+    break
+  else 
+    prompt("Must choose 1, 2, 3 or 4")
+  end
+end
+
+  prompt("#{operation_to_message(operator)} adding the two numbers...")
+  result = case operator
               when '1' 
                 number1.to_i() + number2.to_i()
               when '2' 
@@ -48,14 +93,12 @@ loop do # main loop
                 number1.to_i() * number2.to_i()
               when '4' 
                 number1.to_f() / number2.to_f()
-    end
+  end
 
   prompt("The results is #{result}")
 
   prompt("Do you want to perform another calculation? ( Y to calculate again)") 
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')
-  end
-
-  prompt("Thank you for using the calculator ")
-end 
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+prompt("Thank you for using the calculator! ")
