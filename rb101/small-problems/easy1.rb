4fa53cdfa1d6a8e1c75614ccb78eddd23706e2fa
Abel T <abelt.codes@gmail.com>
2022-02-23 17:18:39 -0700

    small exc

--- rb101/small-problems/easy1.rb
+++ rb101/small-problems/easy1.rb
@@ -1,6 +1,8 @@
 # Repeat Yourself
 # Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
-
+def repeat(str, num) do 
+  puts str * num
+end 
 repeat('Hello', 3)
 # Hello
 # Hello
