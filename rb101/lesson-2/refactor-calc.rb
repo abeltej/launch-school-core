f678c77685c8e31a37303464a3a3736e6805fd42
Abel T <abelt.codes@gmail.com>
2022-02-28 15:31:05 -0700

    refact calc number validation options

--- rb101/lesson-2/refactor-calc.rb
+++ rb101/lesson-2/refactor-calc.rb
@@ -19,6 +19,29 @@ def integer?(input)
   Integer(input) rescue false # use built-in conversion method. In Ruby, there's a method called Kernel#Integer that will convert parameters to the method into an integer object. It will, however, raise a TypeError if the input is not a valid integer, so you'll have to handle that. Note: yes, that's a capitalized method in Ruby -- fortunately, that doesn't happen often.
 end
 
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+def float?(input)
+  input.to_f.to_s == input # This method has an edge case though:
+end
+
+# 2.6.2 :001 > '1.' == '1.'.to_f.to_s
+# => false
+# 2.6.2 :002 > '1.'.to_f
+# => 1.0
+
+# As you can see, to_f converts 1. to 1.0, which does not match the original string.
+
+def float?(input)
+  /\d/.match(input) && /^-?\d*\.?\d*$/.match(input) # Option 2 -- use regex. This regex is similar to the regex in the integer? method, except we have to account for more possible formats. We can combine two validations to verify that the input is a valid float. The first validation verifies that there is at least one digit in the input. The second validation incorporates the * which stands for "zero or more", and the ? which stands for "zero or one". This validation can be read as "zero or more digits, followed by an optional period, followed by zero or more digits. This validation will accept all of these formats: 11.11, 11., .11, but not a period by itself. Notice that we had to prefix the . with a backslash. That is because . matches any single character in regex. By escaping it, we tell Ruby that we are looking for the actual period character.
+end
+
+def float?(input)
+  Float(input) rescue false # use the Kernel#Float method, which is analogous to the Kernel#Integer method from earlier. Just like that method, Float also raises an exception if you don't give it a valid float, so you have to handle it. Note: trailing rescue is a "code smell", so be aware of that. In this specific instance, it's ok, but don't fall into a habit of suppressing errors this way.
+end
+
 def operation_to_message(op)
   case op
   when '1'
