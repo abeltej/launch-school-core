037aad9d7183625e8e844a3c7bc06506c88d8a34
Abel T <abelt.codes@gmail.com>
2022-03-01 10:52:47 -0700

    mortgage

---
+++ rb101/lesson-2/mortgage.rb
@@ -0,0 +1,19 @@
+# Assignment: Mortgage / Car Loan Calculator
+
+# You'll need three pieces of information: 
+
+# the loan amount 
+
+# the Annual Percentage Rate (APR)
+
+# the loan duration
+
+# m = p * (j / (1 - (1 + j)**(-n)))
+
+monthly_payment
+loan_amount
+monthly_interest_rate
+loan_duration_months
+
+
+monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + j)**(-loan_duration_months)))
