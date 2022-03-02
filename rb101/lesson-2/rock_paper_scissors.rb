76608071ee27359d1b1960c056b3599da54fce34
Abel T <abelt.codes@gmail.com>
2022-03-02 16:18:36 -0700

    rock paper scissors start

--- rb101/lesson-2/rock_paper_scissors.rb
+++ rb101/lesson-2/rock_paper_scissors.rb
@@ -1,8 +1,23 @@
+VALID_CHOICES =  ['rock', 'paper', 'scissors']
+
 def prompt(message)
   Kernel.puts("=> message")
 end
 
-prompt("Choice one: rock, paper, scissors")
-choice = Kernel.gets().chomp()
+loop do
+  prompt("Choice one: #{VALID_CHOICES.join(',')}")
+  choice = Kernel.gets().chomp()
 
-computer choice = 
+  if VALID_CHOICES.include?(choice)
+    break
+  else
+    prompt("That's not a valid choice")
+  end
+end
+
+computer_choice = VALID_CHOICES.sample
+
+Kernel.puts("You choose: #{choice}; Computer choose: #{computer_choice}")
+
+if (choice == 'rock' && computer_choice == 'scissors') || (choice == )
+  prompt("You Won!")
