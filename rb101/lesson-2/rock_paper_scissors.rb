e072aad398069f4b639c83591faa541ec3afd721
Abel T <abelt.codes@gmail.com>
2022-03-07 11:55:22 -0700

    rock paper scissors cont

--- rb101/lesson-2/rock_paper_scissors.rb
+++ rb101/lesson-2/rock_paper_scissors.rb
@@ -4,6 +4,7 @@ def prompt(message)
   Kernel.puts("=> message")
 end
 
+choice = ''
 loop do
   prompt("Choice one: #{VALID_CHOICES.join(',')}")
   choice = Kernel.gets().chomp()
@@ -19,5 +20,6 @@ computer_choice = VALID_CHOICES.sample
 
 Kernel.puts("You choose: #{choice}; Computer choose: #{computer_choice}")
 
-if (choice == 'rock' && computer_choice == 'scissors') || (choice == )
+if (choice == 'rock' && computer_choice == 'scissors') || (choice == 'paper' && computer_choice == 'rock') || 
+  (choice == 'scissors' && computer_choice == 'paper')
   prompt("You Won!")
