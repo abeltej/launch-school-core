31e262b44981fc9612d545695e2e682e63c6decf
Abel T <abelt.codes@gmail.com>
2022-03-08 14:49:49 -0700

    rock paper scissors rubocop

--- rb101/lesson-2/rock_paper_scissors.rb
+++ rb101/lesson-2/rock_paper_scissors.rb
@@ -1,21 +1,25 @@
-VALID_CHOICES =  %w('rock', 'paper', 'scissors')
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def display_result(player, computer)
-  if (player == 'rock' && computer == 'scissors') || (player == 'paper' && computer == 'rock') || 
-    (player == 'scissors' && computer == 'paper')
-    prompt("You Won!")
-  elsif ( player == 'rock' && computer == 'paper') || (player == 'paper' && computer == 'scissors') || (player == 'scissors') && (computer == 'rock')
-    prompt('Computer Won!')
-  else 
-    prompt('its a tie')
-  end  
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') || (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
 end
 
-loop do 
+def display_result(player, computer)
+  if win?(player, computer)
+    prompt("You Won!")
+  elsif win?(computer, player)
+    prompt('Computer Won!')
+  else
+    prompt('its a tie')
+  end
+end
+
+loop do
   choice = ''
   loop do
     prompt('Choice one: #{VALID_CHOICES.join(', ')}')
@@ -24,19 +28,19 @@ loop do
     if VALID_CHOICES.include?(choice)
       break
     else
-      prompt('That's not a valid choice')
+      prompt('Thats not a valid choice')
     end
   end
 
   computer_choice = VALID_CHOICES.sample
 
-  prompt('You choose: #{choice}; Computer choose: #{computer_choice}')
+  prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
 
   display_result(choice, computer_choice)
 
   prompt('Do you want to play again?')
   answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?("y")
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt('Thank you for playing')
