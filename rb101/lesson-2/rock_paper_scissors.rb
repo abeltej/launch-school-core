a2c9ff9af0b8b25725ac5222d68e8cf785e95612
Abel T <abelt.codes@gmail.com>
2022-03-08 14:11:58 -0700

    rock paper scissors contt

--- rb101/lesson-2/rock_paper_scissors.rb
+++ rb101/lesson-2/rock_paper_scissors.rb
@@ -1,33 +1,42 @@
 VALID_CHOICES =  ['rock', 'paper', 'scissors']
 
 def prompt(message)
-  Kernel.puts("=> message")
+  Kernel.puts("=> #{message}")
 end
 
 def display_result(player, computer)
   
 end
+loop do 
 
-choice = ''
-loop do
-  prompt("Choice one: #{VALID_CHOICES.join(',')}")
-  choice = Kernel.gets().chomp()
+  choice = ''
+  loop do
+    prompt("Choice one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
   end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You choose: #{choice}; Computer choose: #{computer_choice}")
+
+  if (choice == 'rock' && computer_choice == 'scissors') || (choice == 'paper' && computer_choice == 'rock') || 
+    (choice == 'scissors' && computer_choice == 'paper')
+    prompt("You Won!")
+  elsif ( choice == 'rock' && computer_choice == 'paper') || (choice == 'paper' && computer_choice == 'scissors') || (choice == 'scissors') && (computer_choice == 'rock')
+    prompt('Computer Won!')
+  else 
+    prompt("its a tie")
+  end
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?("y")
 end
 
-computer_choice = VALID_CHOICES.sample
-
-Kernel.puts("You choose: #{choice}; Computer choose: #{computer_choice}")
-
-if (choice == 'rock' && computer_choice == 'scissors') || (choice == 'paper' && computer_choice == 'rock') || 
-  (choice == 'scissors' && computer_choice == 'paper')
-  prompt("You Won!")
-elsif ( choice == 'rock' && computer_choice == 'scissors') || 
-  prompt('Computer Won!')
-else 
-  prompt("its a tie")
+prompt("Thank you for playing")
--- rb101/small-problems/easy1.rb
+++ rb101/small-problems/easy1.rb
@@ -35,13 +35,20 @@ puts is_odd?(7)    # => true
 
 
 # List of Digits
-
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
+
+def digit_list(num)
+  num.to_s.chars.map(&:to_i) # (&:to_i) == { |char| char.to_i }
+end
 
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
+
+
+
+
 
 # How Many?
 # Write a method that counts the number of occurrences of each element in a given array.
