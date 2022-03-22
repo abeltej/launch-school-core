e94af954ead0feb8d9194772fd3905ab26af7c8d
Abel T <abelt.codes@gmail.com>
2022-03-22 13:21:50 -0700

    lesson 3 hard_1 q2

--- rb101/lesson-3/hard_1.rb
+++ rb101/lesson-3/hard_1.rb
@@ -7,7 +7,10 @@ if false
 end
 
 greeting
+
 #  Solution 1
+# greeting is nil here, and no "undefined method or local variable" exception is thrown. Typically, when you reference an uninitialized variable, Ruby will raise an exception, stating that it’s undefined. However, when you initialize a local variable within an if block, even if that if block doesn’t get executed, the local variable is initialized to nil.
+
 #  Question 2
 #  What is the result of the last line in the code below?
 
@@ -19,6 +22,11 @@ puts informal_greeting  #  => "hi there"
 puts greetings
 
 # Solution 2
+# The output is {:a=>"hi there"}. The reason is because informal_greeting is a reference to the original object. The line informal_greeting << ' there' was using the String#<< method, which modifies the object that called it. This means that the original object was changed, thereby impacting the value in greetings. If instead of modifying the original object, we wanted to only modify informal_greeting but not greetings, there are a couple of options:
+
+# we could initialize informal_greeting with a reference to a new object containing the same value by informal_greeting = greetings[:a].clone.
+# we can use string concatenation, informal_greeting = informal_greeting + ' there', which returns a new String object instead of modifying the original object.
+
 # Question 3
 # In other practice problems, we have looked at how the scope of variables affects the modification of one "layer" when they are passed to another.
 
