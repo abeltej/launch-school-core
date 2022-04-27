6fe8a29ac3663421fbfac7c68f36c7234de67ff1
Abel T <abelt.codes@gmail.com>
2022-04-27 15:37:50 -0700

    easy practice problems reverse pt2

--- rb101/small-problems/easy1.rb
+++ rb101/small-problems/easy1.rb
@@ -88,8 +88,6 @@ def reverse_sentence(str)
   str.split.reverse.join(' ')
 end
 
-
-
 # Reverse It (Part 2)
 # Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
 
@@ -97,7 +95,16 @@ puts reverse_words('Professional')          # => lanoisseforP
 puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
 puts reverse_words('Launch School')         # => hcnuaL loohcS
 
+def reverse_words(str)
+ words = []
 
+  str.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
 
 
 
