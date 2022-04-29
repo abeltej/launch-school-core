285ae95b828f9e923fd0546ca5cbc7bf85e9447c
Abel T <abelt.codes@gmail.com>
2022-04-29 09:56:13 -0700

    easy2 practice

--- rb101/small-problems/easy2.rb
+++ rb101/small-problems/easy2.rb
@@ -1,6 +1,9 @@
 # How old is Teddy?
 # Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 200.
-Teddy is 69 years old!
+# Teddy is 69 years old!
+
+age = rand(20..200)
+puts "Teddy is #{age} years old!"
 
 # How big is the room?
 
