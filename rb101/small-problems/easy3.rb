f1b62ec38c6b0bf611fead6563180717ec219b7f
Abel T <abelt.codes@gmail.com>
2022-04-29 13:15:02 -0700

    easy practice 3 numbers

--- rb101/small-problems/easy3.rb
+++ rb101/small-problems/easy3.rb
@@ -1,5 +1,4 @@
 =begin
-
 Searching 101
 Write a program that solicits 6 numbers from the user, then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
 
@@ -33,6 +32,27 @@ The number 17 appears in [25, 15, 20, 17, 23].
 18
 The number 18 does not appear in [25, 15, 20, 17, 23].
 =end
+
+numbers = []
+
+puts "Enter the 1st number:"
+numbers << gets.chomp.to_i
+puts "Enter the 2nd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 3rd number:"
+numbers << gets.chomp.to_i
+puts "Enter the 4th number:"
+numbers << gets.chomp.to_i
+puts "Enter the 5th number:"
+numbers << gets.chomp.to_i
+puts "Enter the last number:"
+last_number = gets.chomp.to_i
+
+if numbers.include?(last_number)
+  puts "#{last_number} appears in #{numbers}"
+else
+  puts "#{last_number}does not appears in #{numbers}"
+end
 
 
 =begin
