f05fb9868bd7a23c120de5b8e43f999ad26d972c
Abel T <abelt.codes@gmail.com>
2022-04-29 11:54:55 -0700

    easy2 practice sqft

--- rb101/small-problems/easy2.rb
+++ rb101/small-problems/easy2.rb
@@ -6,19 +6,29 @@ age = rand(20..200)
 puts "Teddy is #{age} years old!"
 
 # How big is the room?
-
 # Build a program that asks a user for the length and width of a room in meters and then displays the area of the room in both square meters and square feet.
-
 # Note: 1 square meter == 10.7639 square feet
-
 # Do not worry about validating the input at this time.
 
-Enter the length of the room in meters:
-10
-Enter the width of the room in meters:
-7
-The area of the room is 70.0 square meters (753.47 square feet).
+# Enter the length of the room in meters:
+# 10
+# Enter the width of the room in meters:
+# 7
+# The area of the room is 70.0 square meters (753.47 square feet).
 
+SQMETERS_TO_SQFEET = 10.7639
+
+puts '==> Enter the length of the room in meters: '
+length = gets.to_f
+
+puts '==> Enter the width of the room in meters: '
+width = gets.to_f
+
+square_meters = (length * width).round(2)
+square_feet = (square_meters * SQMETERS_TO_SQFEET).round(2)
+
+puts "The area of the room is #{square_meters} " + \
+     "square meters (#{square_feet} square feet)."
 
 # Tip calculator
 
