9bbcee3e18eb17f576275358a7b1415d8bf1a145
Abel T <abelt.codes@gmail.com>
2022-04-29 12:44:06 -0700

    easy practice problems even/odd

--- rb101/small-problems/easy2.rb
+++ rb101/small-problems/easy2.rb
@@ -95,12 +95,19 @@ end
 
 # Odd Numbers
 # Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
-
-
+value = 1
+while value <= 99
+  puts value
+  value += 2
+end
 
 # Even Numbers
 # Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
-
+value = 1
+while value <= 99
+  puts value if value.even?
+  value += 1
+end
 
 
 
