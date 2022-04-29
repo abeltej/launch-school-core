427641120d25c3ea4813f28bed588c7bee290993
Abel T <abelt.codes@gmail.com>
2022-04-29 13:05:29 -0700

    easy practice template

---
+++ rb101/small-problems/easy3.rb
@@ -0,0 +1,159 @@
+=begin
+
+Searching 101
+Write a program that solicits 6 numbers from the user, then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
+
+Examples:
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+17
+The number 17 appears in [25, 15, 20, 17, 23].
+
+
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+18
+The number 18 does not appear in [25, 15, 20, 17, 23].
+=end
+
+
+=begin
+Arithmetic Integer
+Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
+
+Example
+
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 141050039560662968926103
+=end
+
+=begin
+
+Counting the Number of Characters
+Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. Spaces should not be counted as a character.
+
+input:
+
+Please write word or multiple words: walk
+output:
+
+There are 4 characters in "walk".
+input:
+
+Please write word or multiple words: walk, don't run
+output:
+
+There are 13 characters in "walk, don't run".
+=end
+
+=begin
+Multiplying Two Numbers
+Create a method that takes two arguments, multiplies them together, and returns the result.
+
+Example:
+
+multiply(5, 3) == 15
+=end
+
+=begin
+Squaring an Argument
+Using the multiply method from the "Multiplying Two Numbers" problem, write a method that computes the square of its argument (the square is the result of multiplying a number by itself).
+
+Example:
+
+square(5) == 25
+square(-8) == 64
+=end
+
+=begin
+Exclusive Or
+The || operator returns a truthy value if either or both of its operands are truthy. If both operands are falsey, it returns a falsey value. The && operator returns a truthy value if both of its operands are truthy, and a falsey value if either operand is falsey. This works great until you need only one of two conditions to be truthy, the so-called exclusive or.
+
+In this exercise, you will write a function named xor that takes two arguments, and returns true if exactly one of its arguments is truthy, false otherwise. Note that we are looking for a boolean result instead of a truthy/falsy value as returned by || and &&.
+
+Examples:
+
+xor?(5.even?, 4.even?) == true
+xor?(5.odd?, 4.odd?) == true
+xor?(5.odd?, 4.even?) == false
+xor?(5.even?, 4.odd?) == false
+=end
+
+=begin
+
+Odd Lists
+Write a method that returns an Array that contains every other element of an Array that is passed in as an argument. The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+
+Examples:
+
+oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+oddities(['abc', 'def']) == ['abc']
+oddities([123]) == [123]
+oddities([]) == []
+=end
+
+=begin
+Palindromic Strings (Part 1)
+Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
+
+Examples:
+
+palindrome?('madam') == true
+palindrome?('Madam') == false          # (case matters)
+palindrome?("madam i'm adam") == false # (all characters matter)
+palindrome?('356653') == true
+=end
+
+
+=begin
+Palindromic Strings (Part 2)
+Write another method that returns true if the string passed as an argument is a palindrome, false otherwise. This time, however, your method should be case-insensitive, and it should ignore all non-alphanumeric characters. If you wish, you may simplify things by calling the palindrome? method you wrote in the previous exercise.
+
+Examples:
+
+real_palindrome?('madam') == true
+real_palindrome?('Madam') == true           # (case does not matter)
+real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+real_palindrome?('356653') == true
+real_palindrome?('356a653') == true
+real_palindrome?('123ab321') == false
+=end
+
+=begin
+Palindromic Numbers
+Write a method that returns true if its integer argument is palindromic, false otherwise. A palindromic number reads the same forwards and backwards.
+
+Examples:
+
+palindromic_number?(34543) == true
+palindromic_number?(123210) == false
+palindromic_number?(22) == true
+palindromic_number?(5) == true
+=end
