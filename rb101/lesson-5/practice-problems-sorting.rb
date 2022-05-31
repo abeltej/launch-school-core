643e27a30974ac19cb8605b73e5c35271eb01348
Abel T <abelt.codes@gmail.com>
2022-05-31 15:00:51 -0700

    pp 3

--- rb101/lesson-5/practice-problems-sorting.rb
+++ rb101/lesson-5/practice-problems-sorting.rb
@@ -35,14 +35,19 @@ end
 # For each of these collection objects demonstrate how you would reference the letter 'g'.
 
 arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
+arr1[2][1][3]
 
 arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+arr2[1][:third][0]
 
 arr3 = [['abc'], ['def'], {third: ['ghi']}]
+arr3[]
 
 hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
+hsh1{[]}
 
 hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+hsh2{[]}
 
 # Practice Problem 4
 # For each of these collection objects where the value 3 occurs, demonstrate how you would change this to 4.
