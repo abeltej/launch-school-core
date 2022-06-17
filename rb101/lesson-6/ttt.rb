67dc114f5ba40246317912e8d8aca5ad70b08bcf
Abel T <abelt.codes@gmail.com>
2022-06-17 13:07:40 -0700

    ls 6 fixes

--- rb101/lesson-6/ttt.rb
+++ rb101/lesson-6/ttt.rb
@@ -1,8 +1,8 @@
-def display_board
+def display_board(brd)
   puts ""
   puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
-  puts "  #{brd[1]}  | #{brd[2]}   |  #{brd[3]}"
   puts "-----+-----+-----"
   puts "     |     |"
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
@@ -14,9 +14,9 @@ def display_board
   puts ""
 end
 
-def initialize_board(brd)
+def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '}
+  (1..9).each {|num| new_board[num] = 'X'}
   new_board
 end
 
