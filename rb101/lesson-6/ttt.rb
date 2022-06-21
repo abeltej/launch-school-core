a484600740c7a2bf258cf4ebff04825e5e082676
Abel T <abelt.codes@gmail.com>
2022-06-21 07:58:33 -0700

    ls 6 main loop

--- rb101/lesson-6/ttt.rb
+++ rb101/lesson-6/ttt.rb
@@ -44,9 +44,18 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+
 board = initialize_board
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won? || board_full?
+end
