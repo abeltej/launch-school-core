886e1aeb03e4ff5eebbf4953dbf51cd7d3146d5f
Abel T <abelt.codes@gmail.com>
2022-06-23 08:10:37 -0700

    ttt

--- rb101/lesson-6/ttt.rb
+++ rb101/lesson-6/ttt.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -9,38 +11,39 @@ end
 def display_board(brd)
   system 'cls'
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
-  puts ""
-  puts "     |     |"
+  puts ''
+  puts '     |     |'
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
+  puts '     |     |'
+  puts ''
 end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
-  square = ""
+  square = ''
   loop do
     prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry not a valid choice"
+
+    prompt 'Sorry not a valid choice'
   end
 
   brd[square] = PLAYER_MARKER
@@ -60,17 +63,17 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + [[1, 5, 9], [3, 5, 7]]
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], + [[1, 4, 7], [2, 5, 8], [3, 6, 9]], + [[1, 5, 9], [3, 5, 7]]
 
   winning_lines.each do |line|
     if brd[line[0]] == PLAYER_MARKER &&
-      brd[line[1]] == PLAYER_MARKER &&
-      brd[line[2]] == PLAYER_MARKER
-      return "Player"
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
     elsif brd[line[0]] == COMPUTER_MARKER &&
-      brd[line[1]] == COMPUTER_MARKER &&
-      brd[line[2]] == COMPUTER_MARKER
-      return "Computer"
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
     end
   end
   nil
@@ -86,18 +89,18 @@ loop do
     break if someone_won?(board) || board_full?(board)
 
     computer_places_piece!(board)
-   break if someone_won?(board) || board_full?(board)
+    break if someone_won?(board) || board_full?(board)
   end
 
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
   else
-   prompt "its a tie!"
+    prompt 'its a tie!'
   end
 
-  prompt "Play again? ( y or n)"
+  prompt 'Play again? ( y or n)'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thanks for playing Tic Tac Toe! Bye!"
+prompt 'Thanks for playing Tic Tac Toe! Bye!'
