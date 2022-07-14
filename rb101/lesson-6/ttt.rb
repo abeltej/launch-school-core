19c7ccd659f17ee416877910dd45672d300ea379
Abel T <abelt.codes@gmail.com>
2022-07-14 09:09:20 -0700

    defense ai

--- rb101/lesson-6/ttt.rb
+++ rb101/lesson-6/ttt.rb
@@ -91,6 +91,14 @@ def detect_winner(brd)
   nil
 end
 
+def find_at_risk_square(line, board)
+  if board.values_at(*line).count('X') == 2
+    board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+  else
+    nil
+  end
+end
+
 player_score = 0
 computer_score = 0
 loop do
